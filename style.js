4aeb5e16a9cdb2ada85b6a52a110fbdf9690a656
Lenovo <9.3razzinayyuri@gmail.com>
2025-04-28 14:29:50 +0700

    first commit

---
+++ style.js
@@ -0,0 +1,11 @@
+video.addEvenListener("click", function video(){
+    alert("ini video Video Leak Zani");
+});
+
+audio.addEvenListener("click", function audio(){
+    alert("ini audio Video Leak Zani");
+});
+
+gambar.addEvenListener("click", function gambar(){
+    alert("ini gambar Video Leak Zani");
+});
